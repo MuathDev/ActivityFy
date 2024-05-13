811b64a3056d1d477a064d9f685619eeb6277b3b
Muath <muath.developer@gmail.com>
2024-05-13 22:57:20 +0300

    Appling CRUD Using CQRS Mediator pattern

---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,33 @@
+using Application.Activities;
+using Application.Mapping;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddAppServices(this IServiceCollection services,
+             IConfiguration config)
+        {
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            services.AddDbContext<DataContext>(opt =>
+            {
+                opt.UseSqlServer(config.GetConnectionString("DefaultConnection"));
+            });
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(List.Handler).Assembly));
+            services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+            return services;
+
+        }
+    }
+}

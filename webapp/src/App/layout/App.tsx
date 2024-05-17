936069c775f309a88d537b9762e6a99f9a21b543
Muath <muath.developer@gmail.com>
2024-05-17 15:38:20 +0300

    Added new components

--- webapp/src/App/layout/App.tsx
+++ webapp/src/App/layout/App.tsx
@@ -1,31 +1,45 @@
-import { useEffect, useState } from 'react'
-import axios from 'axios';
-import { Header, List } from 'semantic-ui-react';
-import { Activity } from '../models/activity';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Container } from "semantic-ui-react";
+import { Activity } from "../models/activity";
+import NavBar from "./NavBar";
+import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 
 function App() {
-  const[activites,setActivities] = useState<Activity[]>([]);
+  const [activites, setActivities] = useState<Activity[]>([]);
+  const [SelectedActivity, setSelectedActivity] = useState<
+    Activity | undefined
+  >(undefined);
 
-  useEffect(()=>{
-    axios.get<Activity[]>('http://localhost:5000/api/activities')
-    .then(respone=>{
-      setActivities(respone.data)
-    })
-  },[])
+  useEffect(() => {
+    axios
+      .get<Activity[]>("http://localhost:5000/api/activities")
+      .then((respone) => {
+        setActivities(respone.data);
+      });
+  }, []);
 
+  function handleSelectedActivity(id: string) {
+    setSelectedActivity(activites.find((x) => x.id === id));
+  }
+
+  function handleCancelActivity() {
+    setSelectedActivity(undefined);
+  }
 
   return (
-    <div>
-          <Header as='h2' icon='users' content='Activities'/>
-          <List>
-            {activites.map(activity=>(
-              <List.Item key={activity.id}>
-                {activity.title}
-              </List.Item>
-            ))}
-          </List>
-    </div>
-  )
+    <>
+      <NavBar />
+      <Container style={{ marginTop: "7em" }}>
+        <ActivityDashboard
+          activities={activites}
+          selectedActivity={SelectedActivity}
+          selectActivity={handleSelectedActivity}
+          cancelSelectActivity={handleCancelActivity}
+        />
+      </Container>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ webapp/src/features/activities/details/ActivityDetails.tsx
@@ -0,0 +1,49 @@
+import {
+  Button,
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardMeta,
+  Icon,
+  Image,
+} from "semantic-ui-react";
+import { Activity } from "../../../App/models/activity";
+
+interface Props {
+  activity: Activity;
+  cancelSelectActivity: () => void;
+}
+
+export default function ActivityDetails({
+  activity,
+  cancelSelectActivity,
+}: Props) {
+  return (
+    <Card fluid>
+      <Image
+        src={`/assets/categoryImages/${activity.category}.jpg`}
+        wrapped
+        ui={false}
+      />
+      <CardContent>
+        <CardHeader>{activity.title}</CardHeader>
+        <CardMeta>
+          <span className="date">{activity.date}</span>
+        </CardMeta>
+        <CardDescription>{activity.description}</CardDescription>
+      </CardContent>
+      <CardContent extra>
+        <Button.Group widths="2">
+          <Button basic color="blue" content="Edit" />
+          <Button
+            onClick={cancelSelectActivity}
+            basic
+            color="grey"
+            content="Cancel"
+          />
+        </Button.Group>
+      </CardContent>
+    </Card>
+  );
+}
